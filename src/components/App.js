e96ae665ba6d90ba0c8ebb35e38e5556bacf7037
holuwateemeeleyin <abegundeosamuel@gmail.com>
2021-11-19 07:03:24 +0100

    Make changes to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './dashboard'
 import LoadingBar from 'react-redux-loading'
 import NewTweet from './newTweet'
+import TweetPage from './tweetPage'
+import Nav from './nav'
+
 
 class App extends Component {
   // dispatch all the actions that need to be dispatched
-  componentDidMount(){
+  componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
   render() {
     return (
-      <div>
-        <LoadingBar/>
-        {/* if the authedUser is not equal to dashboard, we render null while waiting */}
-        {/* if loading is true, render null, render dashboard if it is not */}
-        { this.props.loading === true
-          ?null
-          : <Dashboard/>
-          // : <NewTweet/>
-        }
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+            <Nav />
+            {/* if the authedUser is not equal to dashboard, we render null while waiting */}
+            {/* if loading is true, render null, render dashboard if it is not */}
+            {this.props.loading === true
+              ? null
+              : <div>
+                <Route path='/' exact component={Dashboard} />
+                <Route path='/tweet/:id' component={TweetPage} />
+                <Route path='/new' component={NewTweet} />
+              </div>
+            }
+          </div>
+        </Fragment>
+      </Router>
     )
   }
 }
 // passed in authedUser as params cos we want to information from authedUser
-function mapStateToProps({ authedUser}){
+function mapStateToProps({ authedUser }) {
   return {
     loading: authedUser === null
   }
